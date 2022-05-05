7f04894ee26cb417c30f005c1f36060c9389d7e5
Leonid <ccleon.id@gmail.com>
2022-05-05 11:21:14 -0300

    P3EJ2b Read and Write by console passed test

--- userland/generaltest.c
+++ userland/generaltest.c
@@ -10,18 +10,20 @@ main(void)
     // Close(o);
     // Remove("test.txt");
 
-    Create("test.txt");
-    OpenFileId o = Open("test.txt");
-    Write("Hello world\n",12,o);
-    Close(o);
+    // Create("test.txt");
+    // OpenFileId o = Open("test.txt");
+    // Write("Hello world\n",12,o);
+    // Close(o);
+    //
+    // char buffer[100];
+    // OpenFileId id = Open("test.txt");
+    // Read(buffer,100, id);
+    // Write(buffer,100,1);
+    // Close(o);
 
     char buffer[100];
-    OpenFileId id = Open("test.txt");
-    Read(buffer,100, id);
+    Read(buffer,100, 0);
     Write(buffer,100,1);
-    Close(o);
-
-
 
     return 0;
 }
